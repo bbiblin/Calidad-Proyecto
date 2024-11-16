91a54efc84d0771c198f141e06189b3603fef1a4
Belén <bele.diaz@duocuc.cl>
2024-11-15 23:20:56 -0300

    2nd commit
    login page

--- DIOX_666/urls.py
+++ DIOX_666/urls.py
@@ -22,4 +22,5 @@ from DioxApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"),
+    path('',views.index, name="index"),
 ]
--- DioxApp/views.py
+++ DioxApp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "pages/home.html")
+
+def index(request):
+    return render(request, "pages/index.html")
